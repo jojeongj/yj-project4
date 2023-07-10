6f14901fa0bea56ce4281f9aeeea368408363f6f
skyisrrna <skyisrrna@naver.com>
2023-07-10 10:09:45 +0900

    jdsl

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,5 +1,6 @@
 import CarouselMain from "./components/CarouselMain";
 import Layout from "./components/Layout";
+import TitleTwoRow from "./components/TitleTwoRow";
 
 export default function Page() {
     return (
@@ -7,6 +8,27 @@ export default function Page() {
             {/* 메인 캐러셀 이미지 */}
             <div className="w-full h-screen-minus-header overflow-hidden">
                 <CarouselMain />
+            </div>
+            {/* 그리드 3/5 2/5 구분 */}
+            <div className="py-16 w-full">
+                <div className="w-full px-4">
+                    <div className="grid grid-cols-5">
+                        {/* 공지사항 */}
+                        <div className="col-span-3 space-y-8">
+                            {/* 타이틀 */}
+                            <TitleTwoRow main="공지사항" sub="notice" />
+                            {/* 공지사항 */}
+                            <div className="w-full flex flex-col">
+                                <div className="border-2 border-neutral-900 w-full flex items-center">
+                                    <div className="text">2023 05-15</div>
+                                    <div>대구 오페라하우스 개관 20주년 이벤트</div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* 아카데미 */}
+                        <div className="col-span-2"></div>
+                    </div>
+                </div>
             </div>
         </Layout>
     )
